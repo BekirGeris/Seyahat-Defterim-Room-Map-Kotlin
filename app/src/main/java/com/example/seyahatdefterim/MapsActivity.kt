96d725e63bb2e2246007e10fd088c24924b1a14e
bekirgeris <bekirgeris@marun.edu.tr>
2021-12-02 23:23:45 +0300

    +maps

--- app/src/main/java/com/example/seyahatdefterim/MapsActivity.kt
+++ app/src/main/java/com/example/seyahatdefterim/MapsActivity.kt
@@ -1,20 +1,39 @@
 package com.example.seyahatdefterim
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.seyahatdefterim.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.seyahatdefterim.databinding.ActivityMapsBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
+    private lateinit var sharedPreferences: SharedPreferences
+    private var trackBoolean: Boolean? = null
+
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +45,78 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        registerLauncher()
+
+        sharedPreferences = this.getSharedPreferences("com.example.seyahatdefterim", MODE_PRIVATE)
+        trackBoolean = false
     }
 
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.setOnMapClickListener(this)
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
+
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(p0: Location) {
+                trackBoolean = sharedPreferences.getBoolean("trackBoolean", false)
+                if (trackBoolean == false){
+                    val userLocation = LatLng(p0.latitude, p0.longitude)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+                    sharedPreferences.edit().putBoolean("trackBoolean", true).apply()
+                }
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                //izin ikinci kere isteniyor.
+                Snackbar.make(binding.root, "Permission needed for location", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission") {
+                    //izin istenecek
+                    permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }.show()
+            }else{
+                //izin ilk defa isteniyor
+                permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }else{
+            //izin var
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+            val lastLocation  = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (lastLocation != null){
+                val lastUserLocation = LatLng(lastLocation.latitude, lastLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15f))
+            }
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun registerLauncher(){
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (result) {
+                //permission granted
+                if (ContextCompat.checkSelfPermission(this@MapsActivity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+                    val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if (lastLocation != null) {
+                        val lastUserLocation = LatLng(lastLocation.latitude, lastLocation.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15f))
+                    }
+                    mMap.isMyLocationEnabled = true
+                }
+            }else{
+                //izin verilmedi
+                Toast.makeText(this@MapsActivity, "Permision needed!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onMapClick(p0: LatLng) {
+
+        mMap.clear()
+        mMap.addMarker(MarkerOptions().position(p0))
     }
 }
